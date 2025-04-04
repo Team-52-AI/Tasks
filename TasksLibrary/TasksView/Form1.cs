1015a5082a5d5b21ad1566e03d612036490ddb62
Дмитрий Лёвкин <jonikm892@gmail.com>
2025-04-04 10:07:04 +0300

    Доработка интерфейса + тесты

--- TasksLibrary/TasksView/Form1.cs
+++ TasksLibrary/TasksView/Form1.cs
@@ -16,58 +16,5 @@ namespace TasksView
         {
             InitializeComponent();
         }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            Reference form = new Reference();
-            form.ShowDialog();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Task1 form = new Task1();
-            form.ShowDialog();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Task2 form = new Task2();
-            form.ShowDialog();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            Task3 form = new Task3();
-            form.ShowDialog();
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            Task4 form = new Task4();
-            form.ShowDialog();
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            Task5 form = new Task5();
-            form.ShowDialog();
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            Task6 form = new Task6();
-            form.ShowDialog();
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            Task7 form = new Task7();
-            form.ShowDialog();
-        }
     }
 }
